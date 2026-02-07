effe530737a67202aed838cf4ef9f92c7a007a42
yasin <yasinsheikhofficial@gmail.com>
2026-02-07 11:24:46 +0530

    footer added

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,7 @@ export default function RootLayout({ children }) {
           {/*footer*/}
           <footer className="bg-muted/50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>footer</p>
+              <p>© 2024 Senpai AI Career Coach. All rights reserved.</p>
             </div>
           </footer>
         </ThemeProvider>
